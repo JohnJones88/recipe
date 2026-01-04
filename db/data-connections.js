40b68c4553091ce5aa4fae7195da3ef8d7c1b828
John Jones <jajones0622@gmail.com>
2026-01-04 00:17:15 -0500

    change db process to .env

--- db/data-connections.js
+++ db/data-connections.js
@@ -1,15 +1,15 @@
 const { Sequelize } = require('sequelize');
 
-const sequelize = new Sequelize ('recipe_db', 'root', 'password',{
-    host: 'localhost',
+const sequelize = new Sequelize (process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
   dialect: 'mysql'
-
 });
 
 const connectToDb = async () => {
     try {
   
-      sequelize.sync({ force: false });
+      sequelize.sync({ force: process.env.DB_FORCE_UPDATE });
   
       await sequelize.authenticate();
       console.log("Connected to Database")
